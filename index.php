ce6ac59a75bbb7da40a24d74e432f6aed87d6e7a
Rockenstein <paras.klangyan@gmail.com>
2019-11-23 10:22:02 +0530

    bootstrap added

--- index.php
+++ index.php
@@ -4,30 +4,62 @@ require_once('includes/dbconnect.php');
 //if no or 0 user bug
 require_once('includes/session.php');
 require_once('login/usercheck.php');
-require_once('includes/navbar.html');
+require_once('includes/navbar.php');
 if(!$cs->redirect())  header("location:login.php"); //got false, converted true to redirect to login because can't find any session
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <title>Document</title>
+  
     <style>
         table{
             text-align:center;
             height:200px;
         }
+        *{
+            overflow:auto;
+        }
         input[type="text"],textarea {
-         width: 100%; 
-         height:100%;
+         width: 50%; 
+         height:50%;
+         margin:0px auto;
+        }
+        input, textarea, label{
+            vertical-align:middle;
         }
         .tdispi{
             display:none;
         }
+        #m_name,#desc,#date,#wp,#stock,#discount,#mrp{
+            height:40px;
+            width:100%
+        }
+        
     </style>
     <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+    <script src="http://code.jquery.com/ui/1.10.0/jquery-ui.js"></script>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="/resources/demos/style.css">
+  
+  
     <script>
+     
     $(document).ready(function(){
         
+        $( function() {
+        
+        var numItems =  $('.accord_class').length;
+        var i=1;
+        while(i <= numItems){
+            $("#description_accordian" + i).accordion({
+                active: 2,
+                animate: 200,
+                collapsible: true
+            });
+            i++;
+        }
+  } );
         //alert(numItems);
         $(function () {
             var numItems = $('.cart_change').length;
@@ -114,6 +146,7 @@ if(!$cs->redirect())  header("location:login.php"); //got false, converted true 
         $(function () {
             var numItems = $('.emc').length;
             for(let i = 1; i <= numItems; i++) {
+                $(".start_disp"+i).css({'display':'none'});
                 $('#edit_mob' + i).click( function(){
                     
                     //Assigning values to textarea to edit
@@ -126,15 +159,16 @@ if(!$cs->redirect())  header("location:login.php"); //got false, converted true 
                     //$("#date" +i).val("2018-12-19");
                     //$("#date" +i).val(date_replace);
                     //$("b").append(date_replace);
-                    $("#wp" +i).text($("#wpi" +i).text().match(/(\d+)/)[0]);
-                    $("#stock" +i).text($("#stocki" +i).text().match(/(\d+)/)[0]);
-                    $("#discount" +i).text($("#discounti" +i).text().match(/(\d+)/)[0]);
-                    $("#mrp" +i).text($("#mrpi" +i).text().match(/(\d+\,\d+)|(\d+)/)[0]);
+                    $("#wp" +i).val($("#wpi" +i).text().match(/(\d+)/)[0]);
+                    $("#stock" +i).val($("#stocki" +i).text().match(/(\d+)/)[0]);
+                    $("#discount" +i).val($("#discounti" +i).text().match(/(\d+)/)[0]);
+                    $("#mrp" +i).val($("#mrpi" +i).text().match(/(\d+\,\d+)/)[0]);
                     //####################################
                     //Assigning above values to variable to send
                     //$("selbid"+i+" option[value='"+bid+"']").attr("selected","selected");
                     //####################################
                     $(".dn" +i).css({'display':'none'});
+                    $(".start_disp"+i).css({'display':'block'});
                     $(".tdisp" +i).css({'display':'inline'});
                     $("#edit_mob" + i).text("Update");
                     //$("b").text(bid);
@@ -167,118 +201,158 @@ if(!$cs->redirect())  header("location:login.php"); //got false, converted true 
                 });
                 
             }
-    });
+                
+                
+
+            /**/
+            
+            
+        });
 });
     
     </script>
 </head>
 <body>
 <b></b>
-    <p align="right"><?php echo $cs->getUsername();?></p>
-    <a href="logout.php">logout?</a>
-    <div>
-        <ul>
-            <li><a href="index.php">Dashboard</a></li>
-            <?php if($aon){
-                       echo "<li><a href=userinfo.php>View User Info</a></li>";                
-            }
-            else{
-                    echo "<li><a href=viewcart.php>View Cart</a></li>";
-            }?>
-            <li><a href="vieworders.php">View All orders</a></li>
-            <li><a href="suggestion.html"> <?php echo ($aon) ? "View Suggestions" : "Suggest" ; ?></a></li>
-        </ul>
-    </div>
-    <table border=1>
-        <tr>
-            <th>Brand</th>
-            <th>Mobile Name</th>
-            <th>Preview</th>
-            <th>Description</th>
-            <th>Manufacturing Date</th>
-            <th>Warranty (Months)</th>
-            <th>Stock</th>
-            <th>Discount</th>
-            <th>Market Price</th>
-            <?php
-                if(!$aon){
-                    echo "<th>Add to cart?</th>"; 
-                }
-                else if($aon){
-                    echo "<th>Add or Edit</th>";
-                }
-            ?>
-            
-           
-        </tr>
-        <?php
-            $id = 1;
-            $query = $con->connect()->prepare("SELECT * FROM `mobile_brands` WHERE 1");
-            $query->execute();
-            $resbnames = $query->fetchAll();
-            $query = $con->connect()->prepare("CALL fetch_mobiles_brand");
-            $query->execute();
-            //$query = mysqli_query($con,"CALL fetch_mobiles_brand");
-            while($res = $query->fetch()){
-                echo"<tr>";
-                echo"<td>";
-                
-                echo"<select id=selbid".$id." class=\"tdispi tdisp".$id."\" required>";
-                echo "<option value=>Please Select</option>";
-                
+
+<div class="container">
+   
+<?php
+    $id = 1;
+    $query = $con->connect()->prepare("SELECT * FROM `mobile_brands` WHERE 1");
+    $query->execute();
+    $resbnames = $query->fetchAll();
+    $query = $con->connect()->prepare("CALL fetch_mobiles_brand");
+    $query->execute();
+    while($res = $query->fetch()){
+?>
+    <div class="card mb-3">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="..." class="card-img" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $res['b_Name']." - ".$res['mobile_name']; ?></h5>
+                        <div class="accord_class dn<?php echo $id;?>" id="description_accordian<?php echo $id;?>">
+                            <h3>1. Description</h3>
+                            <div>
+                                <p><?php echo $res['description'];?></p>
+                            </div>
+                            <h3>2. Buying Information</h3>
+                            <div>
+                                <p><?php echo"<b>Manufacturing Date - </b> <span id=datei".$id.">".$res['man_date']."</span><br><b>Warranty - </b> <span id=wpi".$id.">".$res['warranty']."</span>"?></p>
+                            </div>
+                        </div>
+                        <p id=mrpi<?php echo $id; ?> class="catd-text"><b>MRP - &#8377;<?php echo $res['mrp'];?></b></p>
+                        <p id=stocki<?php echo $id; ?> class="card-text"><small class="text-muted">Only <?php echo $res['stock']?> left!</small></p>
+                        <div class="form-group start_disp<?php echo $id;?>">
+                        <?php
+
+                            echo"<select id=selbid".$id." class=\"tdispi form-control tdisp".$id."\" required>";
+                            echo "<option value=>Please Select</option>";
                             foreach($resbnames as $rows){
                                 echo "<option value=".$rows['b_id'].">".$rows['b_Name']."</option>";
                             }
-                echo"</select>";
-                
-                
-                echo "<p class=dn".$id.">".$res['b_Name']."</p></td>";
-                echo"<td><textarea id=m_name".$id." cols=30 rows=10 class=\"tdispi tdisp".$id."\"></textarea><p id=m_namei".$id." class=dn".$id.">".$res['mobile_name']."</p></td>";
-                echo"<td> <img class=dn".$id." src=".$res['pic']." alt=?> </td>";
-                echo"<td><textarea id=desc".$id." cols=30 rows=10 class=\"tdispi tdisp".$id."\"></textarea><p id=desci".$id." class=dn".$id.">".$res['description']."</p></td>";
-                echo"<td> <input type=date id=date".$id." class=\"tdispi tdisp".$id."\"> <p id=datei".$id." class=dn".$id."> ".$res['man_date']."</p></td>";
-                echo"<td><textarea id=wp".$id." cols=30 rows=10 class=\"tdispi tdisp".$id."\"></textarea><p id=wpi".$id." class=dn".$id.">".$res['warranty']." months</p></td>";
-                echo"<td><textarea id=stock".$id." cols=30 rows=10 class=\"tdispi tdisp".$id."\"></textarea><p id=stocki".$id." class=dn".$id.">".$res['stock']." left!</p></td>";
-                echo"<td><textarea id=discount".$id." cols=30 rows=10 class=\"tdispi tdisp".$id."\"></textarea><p id=discounti".$id." class=dn".$id.">".$res['discount']."%</p></td>";
-                echo"<td><textarea id=mrp".$id." cols=30 rows=10 class=\"tdispi tdisp".$id."\"></textarea><p id=mrpi".$id." class=dn".$id."> &#8377;".$res['mrp']."</p></td>";
-                if(!$aon){
-                    echo"<td> <button class=cart_change id=m".$id." value='".$res['mobile_name']."'>Add</button></td>";    
-                }
-                else if($aon){
-                    echo"<td><button class=emc id=edit_mob".$id.">Edit</button</td>";
-                }
-                echo"</tr>";
-                echo "<input type=hidden value=".$id.">";
-                $id++;
-            }
-            if($aon){
-            ?>
-                <tr>
-                <td>
-                   <select id="selbid" required>
-                        <option value="">Please Select</option>
-                        <?php
-                        $query = $con->connect()->prepare("SELECT * FROM `mobile_brands` WHERE 1");
-                        $query->execute();
-                            while($res = $query->fetch()){
-                                echo "<option value=".$res['b_id'].">".$res['b_Name']."</option>";
-                            }
+                            echo"</select>";
                         ?>
-                    </select>
-                </td>
-                <td><textarea name="" id="m_name" cols="30" rows="10" placeholder="Enter Mobile Name" required></textarea></td>
-                <td id="pic" value="#">In progrss!</td>
-                <td><textarea name="" id="desc" cols="30" rows="10" placeholder="Enter Description" required></textarea></td>
-                <td> <input type="date" name="" id="date" required> </td>
-                <td><textarea name="" id="wp" cols="30" rows="10" placeholder="Enter Warranty Period (Only number)" required></textarea></td>
-                <td><textarea name="" id="stock" cols="30" rows="10" placeholder="Enter In stock(in numbers)" required></textarea></td>
-                <td><textarea name="" id="discount" cols="30" rows="10" placeholder="Enter Discount(Numbers)" required></textarea></td>
-                <td><textarea name="" id="mrp" cols="30" rows="10" placeholder="Enter Mrp in (Numbers)" required></textarea></td>
-                <td><button id="add_mob">Add</button></td>
-            </tr>
-            <?php
-            } //end of user check and add table
-            ?>
-    </table>
+                        </div>
+                        <div class="tdispi form-group start_disp<?php echo $id;?>">
+                            <label for=m_name<?php echo $id;?>>Mobile Name :</label>
+                            <input class="form-control" id=m_name<?php echo $id;?> class="tdispi tdisp<?php echo $id;?>" >
+                        </div>
+                        <div class="tdispi form-group start_disp<?php echo $id;?>">
+                            <label for=desc<?php echo $id;?>>Description :</label>
+                            <textarea class="form-control" id=desc<?php echo $id;?> cols=30 rows=10 class="tdispi tdisp<?php echo $id;?>"></textarea>
+                        </div>
+                        <div class="tdispi form-group start_disp<?php echo $id;?>">
+                            <label for=date<?php echo $id;?>>Date :</label>
+                            <input class="form-control" type=date id=date<?php echo $id;?> class="tdispi tdisp<?php echo $id;?>">
+                        </div>
+                        <div class="tdispi form-group start_disp<?php echo $id;?>">
+                            <label for=wp<?php echo $id;?>>Warranty :</label>
+                            <input class="form-control" id=wp<?php echo $id;?> class="tdispi tdisp<?php echo $id;?>">
+                        </div>
+                        <div class="tdispi form-group start_disp<?php echo $id;?>">
+                            <label for=stock<?php echo $id;?>>Stocks :</label>
+                            <input class="form-control" id=stock<?php echo $id;?> class="tdispi tdisp<?php echo $id;?>">
+                        </div>
+                        <div class="tdispi form-group start_disp<?php echo $id;?>">
+                            <label for=discount<?php echo $id;?>>Discount :</label>
+                            <input class="form-control" id=discount<?php echo $id;?>  class="tdispi tdisp<?php echo $id;?>">
+                        </div>
+                        <div class="tdispi form-group start_disp<?php echo $id;?>">
+                            <label for=mrp<?php echo $id;?>>Mrp :</label>
+                            <input class="form-control" id=mrp<?php echo $id;?> class="tdispi tdisp<?php echo $id;?>">
+                        </div>
+                        
+                        <?php
+                        if(!$aon){
+                            echo"<button class=\"btn btn-primary cart_change\" id=m".$id." value='".$res['mobile_name']."'>Add</button>";    
+                        }
+                        else if($aon){
+                            echo"<button class=\"btn btn-primary emc\" id=edit_mob".$id.">Edit</button>";
+                        }
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>    
+      
+    <?php $id++;    } 
+    
+    if($aon){?>
+    <div class="card text-white bg-primary mb-3">
+    <div class="card-header">Add More Phones</div>
+    <div class="card-body">
+    <h5 class="card-title">Primary card title</h5>
+    <div class="form-group">
+    <select id="selbid" class="form-control"required>
+        <option value="">Please Select</option>
+        <?php
+            $query = $con->connect()->prepare("SELECT * FROM `mobile_brands` WHERE 1");
+            $query->execute();
+            while($res = $query->fetch()){
+                echo "<option value=".$res['b_id'].">".$res['b_Name']."</option>";
+            }
+        ?>
+    </select>
+    </div>
+    <div class="form-group">
+        <label for=m_name>Mobile Name:</label>
+        <input class="form-control" type=text id=m_name >
+    </div>
+    <div class="form-group">
+        <label for=desc>Description :</label>
+        <textarea class="form-control" id=desc></textarea>
+    </div>
+    <div class=" form-group">
+        <label for=date>Date :</label>
+        <input class="form-control" type=date id=date>
+    </div>
+    <div class=" form-group">
+        <label for=wp>Warranty :</label>
+        <input class="form-control" type=text id=wp>
+    </div>
+    <div class=" form-group">
+        <label for=stock>Stocks :</label>
+        <input class="form-control" type=text id=stock>
+    </div>
+    <div class="form-group">
+        <label for=discount>Discount :</label>
+        <input class="form-control" type=text id=discount>
+    </div>
+    <div class=" form-group">
+        <label for=mrp>Mrp :</label>
+        <input class="form-control" type=text id=mrp>
+    </div>
+    <button id="add_mob" class="btn btn-secondary">Add</button>
+  </div>
+</div><?php }?>
+
+   
+
+</div>    
+    
 </body>
 </html>
--- viewcart.php
+++ viewcart.php
@@ -2,6 +2,9 @@
 <?php
     require_once('includes/dbconnect.php');
     require_once('includes/session.php');
+    require_once('login/usercheck.php');
+    require_once('includes/navbar.php');
+    
     if(!$cs->redirect())  header("location:login.php"); //got false, converted true to redirect to login because can't find any session
 ?>
 <!DOCTYPE html>
@@ -29,53 +32,74 @@
 </head>
 <body>
     <p></p>
-    <form action="cart/add2cart.php?control=remove&m=rm_v" method="post">
-    <table border=1>
-        <tr>
-            <th>Brand</th>
-            <th>Mobile Name</th>
-            <th>Preview</th>
-            <th>Description</th>
-            <th>Manufacturing Date</th>
-            <th>Warranty (Months)</th>
-            <th>Stock</th>
-            <th>Discount</th>
-            <th>Market Price</th>
-            <th>Remove?</th>
-        
-        </tr>
+    <div class="container">
+                <div class="row">
         <?php
-            $id = 1;$row_present = 0;
+        
+            $id = 1;$row_present = 0;$each_device_money = 0;$total_money = 0;
             $query = $con->connect()->prepare("CALL get_cid_seca_secq(?)");
             $query->execute([$_SESSION['email']]);
             $res_cid = $query->fetchAll();
             $query = $con->connect()->prepare("SELECT mobile_data.*, mobile_brands.b_Name FROM mobile_data INNER JOIN cart on mobile_data.m_id = cart.m_id JOIN mobile_brands on mobile_data.b_id = mobile_brands.b_id where cart.c_id = ?"); 
             $query->execute([$res_cid[0]['c_id']]);
-            while($res = $query->fetch()){
-                $row_present = 1; //flag for query returning not null
-                    echo"<tr>";
-                    echo"<td name=brand>".$res['b_Name']."</td>";
-                    echo"<td name=val>".$res['mobile_name']."</td>";
-                    echo"<td name=pic> <img src=".$res['pic']." alt=?> </td>";
-                    echo"<td name=des>".$res['description']."</td>";
-                    echo"<td name=mdate>".$res['man_date']."</td>";
-                    echo"<td name=wnty>".$res['warranty']." months</td>";
-                    echo"<td name=stck> ".$res['stock']." left!</td>";
-                    echo"<td name=discount>".$res['discount']."%</td>";
-                    echo"<td name=mrp> &#8377;".$res['mrp']."</td>";
-                    echo"<input type=hidden name=val value='".$res['mobile_name']."'>";
-                    echo"<td> <input type=submit value=Remove></td>";
-                    echo"</tr>";
+            while($res = $query->fetch()){ $row_present = 1;
+                $each_device_money = str_replace( ',', '', $res['mrp'] );
+                $each_device_discount = str_replace(',','', $res['discount']);
+                $after_discount_money = (int)$each_device_money*((int)$each_device_discount/100);
+            ?>
+                
+                <form action="cart/add2cart.php?control=remove&m=rm_v" method="post">
+                    <div class="card mr-2" style="width: 20em;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                        <h5 class="card-title"><?php echo $res['b_Name']." - ".$res['mobile_name'];?></h5>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item"><b>Warranty - </b><?php echo $res['warranty'];?> months</li>
+                        <li class="list-group-item"><b>Stock - </b><?php echo $res['stock'];?> left!</li>
+                        <li class="list-group-item"><b>Discount - </b><?php echo $res['discount'];?>%</li>
+                        <li class="list-group-item"><b>M.R.P - &#8377;</b><span class="disabled" style="text-decoration:line-through"><?php echo $res['mrp'];?></span>
+                        <span style="margin-left:10px;">-  <b>&#8377;</b><?php echo number_format($after_discount_money);?></span></li>
+                    </ul>
+                    <input type="hidden" name=val value="<?php echo $res['mobile_name'];?>">
+                    <div class="card-body">
+                        <input class="btn btn-primary centre" type="submit" value=Remove>
+                    </div>
+                    </form>
+                
+
+                
+</div>
+            <?php     //flag for query returning not null
+                    
+                    
+                    
+                    $total_money +=$after_discount_money;
                     $id++;
             }
+            
             if(!$row_present){
-                echo"<tr>";
-                echo "<td colspan=10 align=center>Your Cart is empty!! Buy some interesting mobiles, OR check the view all orders if you have bought already!! :)</td>";
-                echo"</tr>";
+                echo "<div class=\"alert alert-danger\" role=\"alert\">Your Cart is empty!! Buy some interesting mobiles, OR check the view all orders if you have bought already!! :)</div>";
+            }else{
+                ?>
+                </div>
+                </div>
+                <div class="card text-white bg-success mb-3 text-center">
+                <div class="card-header ">Total Bill</div>
+                <div class="card-body">
+                <h5 class="card-title">Items - <?php echo --$id;?></h5>
+                <p class="card-text"><?php echo " &#8377;". number_format($total_money);?></p>
+                </div>
+                
+                <?php
+                
+                echo "<button class=\"btn btn-primary\" id=\"checkout\">Checkout All</button>";
             }
+            
         ?>
-    </table>
-        </form>
-    <button id="checkout">Checkout All</button>
+    
+        
+    
+
 </body>
 </html>     
